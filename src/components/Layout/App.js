1273a01dc29d92d903769072d90f45bf05058c95
Davi Alves <hey@davialves.com>
2021-06-23 23:44:59 -0300

    feat(housekeeping): added prettier, husky, lint-staged and standard-version; organized folders into DDD structure

--- src/components/Layout/App.js
+++ src/components/Layout/App.js
@@ -1,12 +1,11 @@
+import React from 'react';
 import './App.css';
-import Feed from './components/feed/Feed';
-import Queue from './components/queue/Queue';
-import Iframe from './components/iframe/Iframe';
-
+import Feed from '../../features/Feed/Feed';
+import Queue from '../../features/Queue/Queue';
+import Iframe from '../Iframe/Iframe';
 
 function App() {
-
-  const sobre = "Um pouco sobre mim"
+  const sobre = 'Um pouco sobre mim';
 
   return (
     <div className="App">
@@ -15,19 +14,21 @@ function App() {
           <div className="col-sm">
             <div className="w-500 mw-full">
               <div className="card">
-                <img src="https://sm.ign.com/ign_br/screenshot/default/agos_ye78.jpg" className="img-fluid" alt="" />
+                <img
+                  src="https://sm.ign.com/ign_br/screenshot/default/agos_ye78.jpg"
+                  className="img-fluid"
+                  alt=""
+                />
                 <div className="content">
-                  <h2 className="content-title">
-                    {sobre}
-                  </h2>
+                  <h2 className="content-title">{sobre}</h2>
                   <p>
-                  This textarea is read-only, so it can not be edited by the user. It can only be read.
+                    This textarea is read-only, so it can not be edited by the
+                    user. It can only be read.
                   </p>
                 </div>
                 <hr />
               </div>
             </div>
-
           </div>
           <div className="col-sm">
             <Feed />
